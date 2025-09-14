619b3b8591f250c8802dd84b96d06f876d582c58
Moses Butera <152932522+MosesButera@users.noreply.github.com>
2025-09-14 10:19:04 -0600

    Validate string input Add return that ensures the first argument is actually a string. Without this, methods like .chars would through errors on invalid types (like numbers or arrays).

--- index.rb
+++ index.rb
@@ -1,6 +1,8 @@
 
 def caesar_cipher(my_string, shift = nil)
   return "Please enter shift_factor" if shift_factor.nil?
+  return "Input/my_string parameter must be a string" unless my_string.is_a?(String)
+  
 
 end
 
