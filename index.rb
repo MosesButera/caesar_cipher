4ce990037770bc14b1a636b9d77669ee358ad78f
Moses Butera <152932522+MosesButera@users.noreply.github.com>
2025-09-14 10:21:14 -0600

    Validate shift factor type Add early return to ensure the shift factor is a number. This prevents runtime issues with string or float inputs.

--- index.rb
+++ index.rb
@@ -2,6 +2,7 @@
 def caesar_cipher(my_string, shift = nil)
   return "Please enter shift_factor" if shift_factor.nil?
   return "Input/my_string parameter must be a string" unless my_string.is_a?(String)
+  return "Input/shift_factor must be an Integer" unless shift_factor.is_a?(Integer)
   
 
 end
