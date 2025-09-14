69058acb8f1daaa317f6a101df610841eeea1ba8
Moses Butera <152932522+MosesButera@users.noreply.github.com>
2025-09-14 10:22:37 -0600

    Split string into characters Convert the input stirng into an array of characters. Example: "abc" to ["a", "b", "c"]. This allows us to transform each character individually using .map method.

--- index.rb
+++ index.rb
@@ -3,7 +3,9 @@ def caesar_cipher(my_string, shift = nil)
   return "Please enter shift_factor" if shift_factor.nil?
   return "Input/my_string parameter must be a string" unless my_string.is_a?(String)
   return "Input/shift_factor must be an Integer" unless shift_factor.is_a?(Integer)
-  
+
+  string = my_string.chars    #convert string into array made up on 
+  #the string characters so we can use map method on them.
 
 end
 
